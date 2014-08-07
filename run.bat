a8c064eb22140d22e072a99a2615ddc57cfbe2ab
dboissin <damien.boissin@web-education.net>
2014-08-07 18:13:41 +0200

    version 1.8-SNAPSHOT

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 SET PATH=%PATH%;tools\vertx\bin;tools\gradle\bin;tools\jdk\bin
 
 tasklist /nh /fi "imagename eq mongod.exe" | find /i "mongod.exe" > nul || (start cmd /k tools\mongodb\bin\mongod.exe --dbpath tools\mongodb-data)
-vertx runMod org.entcore~infra~1.6-SNAPSHOT -conf ent-core.embedded.json
+vertx runMod org.entcore~infra~1.8-SNAPSHOT -conf ent-core.embedded.json
