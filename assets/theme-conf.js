3a876ca768bfd759245ffcab74a3586efd3d2e7a
Clement Creusat <clement.creusat@gmail.com>
2023-09-28 15:23:08 +0200

    update springboard front dependencies

---
+++ assets/theme-conf.js
@@ -0,0 +1,11 @@
+exports.conf = {
+  overriding: [
+    {
+      parent: "theme-open-ent",
+      child: "neoconnect",
+      group: "neoconnect",
+      skins: ["default", "dyslexic"],
+      bootstrapVersion: "ode-bootstrap-neo",
+    },
+  ],
+};
